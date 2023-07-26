8783bcea7b9d49f75c9e58193cc45f57e72eb130
khemrajsinght <khemrajsinght57@gmail.com>
2023-07-26 11:30:43 +0545

    flutter v1

---
+++ lib/src/screen/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:skin_disease_prediction/src/screen/login_screen.dart';
+import 'package:skin_disease_prediction/src/screen/onboarding.dart';
+
+class SplashScreen extends StatefulWidget {
+  int? isviewed;
+  SplashScreen({super.key, this.isviewed});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logo: Image.asset(
+        "assets/logo/logo.png",
+      ),
+      title: Text(
+        "Skin Disease Prediction",
+        style: GoogleFonts.poppins(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      backgroundColor: Colors.white54,
+      showLoader: true,
+      loaderColor: Colors.indigo,
+      navigator:
+          widget.isviewed != 0 ? const OnBoardingScreen() : const LoginScreen(),
+      durationInSeconds: 4,
+    );
+  }
+}
